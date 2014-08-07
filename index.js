65b176a868b98c468cfc4bf887b18ace94e63a23
piousbox <piousbox@gmail.com>
2014-08-06 18:59:49 -0700

    going over the tutorial

--- index.js
+++ index.js
@@ -3,18 +3,34 @@
 'use strict';
 
 /**
- * blah blah
- **/
+ * Practicing building stuff with Socket.io
+ * Author: piousbox
+ * No copyright. 2014
+ *
+ */
 
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 var config = { port: 3001 };
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket) {
+    console.log('a user connected');
+
+    socket.on('disconnect', function(){
+	console.log('user disconnected');
+    });
 });
 
 http.listen(config.port, function(){
   console.log('listening on *:' + config.port);
 });
+
+
+
+
